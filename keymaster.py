ad99f7402bba6ced95d99918e1fea3389aa46524
Scott Odle <scott@sjodle.com>
2015-11-27 15:15:49 -0700

    Working

---
+++ keymaster.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import requests
+import argparse
+import sys
+
+key_upload_url = 'http://keymaster.sjodle.com/k'
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload SSH key to Keymaster")
+    parser.add_argument('filename', help='Public keyfile to upload.')
+    args = parser.parse_args()
+
+    try:
+        print("Uploading key " + args.filename)
+        with open(args.filename) as key_file:
+            public_key = key_file.read()
+            key_req = requests.post(key_upload_url,
+                                    data={'public_key': public_key})
+            key_req.raise_for_status()
+            print("Your key has been uploaded successfully!")
+            print("View or install it from any browser on any device:")
+            print(key_req.text)
+    except IOError as error:
+        sys.exit('File not found: ' + filename)
